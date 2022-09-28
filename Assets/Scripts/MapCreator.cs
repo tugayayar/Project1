7ccf805b65ea319d93f9c4a3ff1c572888054000
Tugay Ayar <tugayayar@hotmail.com>
2022-09-28 19:08:58 +0300

    Added: Map Creator Mechanic With Button

--- Assets/Scripts/MapCreator.cs
+++ Assets/Scripts/MapCreator.cs
@@ -14,65 +14,93 @@ public class MapCreator : MonoBehaviour
     #endregion
 
     [Header("Map Variables")]
-    private int size = 5;
+    private int size = 3;
     public int width;
     public int height;
-    public float cellSize;
+    public Vector2 cellSize;
     public int[,] mapArray;
+    float screenHeightPixelValue = 828f;//toplamda 828 pixel'lik bir boþluk býrakacak ekranýn altýndan
+    float buttonSizeMult = 3.73f;
+    //cellSize.x / 3.73f = button width
+    //cellSize.y / 3.73f = button height
 
     [Header("Map Part")]
-    [SerializeField] private Transform mapPart;
+    [SerializeField] private RectTransform mapPart;
+
+    [Header("Map Canvas")]
+    [SerializeField] private RectTransform mapCanvas;
+    [SerializeField] private RectTransform gameInPanel;
+    [SerializeField] private Camera cam;
  
     private void Start()
     {
         width = size;
         height = size;
-        cellSize = size;
-        CreateMap(width, height, cellSize);
+        cellSize = CalculateCellSize();
+        CreateMap(width, height);
+        
+    }
+    
+    private Vector2 CalculateCellSize()
+    {
+        float w = Screen.width / size;
+        float h = (Screen.height - screenHeightPixelValue) / size;
+        return new Vector2(w, h);
     }
 
-    public void CreateMap(int width, int height, float cellSize)
+    public void CreateMap(int width, int height)
     {
         mapArray = new int[width, height];
 
         DrawMap();
     }
-
+    
     void DrawMap()
     {
         for (int x = 0; x < mapArray.GetLength(0); x++)
         {
-            for (int z = 0; z < mapArray.GetLength(1); z++)
+            for (int y = 0; y < mapArray.GetLength(1); y++)
             {
-                //Transform txt = Instantiate(textt, GetWorldPosition(x, z), Quaternion.identity);
-                //txt.GetChild(0).GetComponent<TextMeshPro>().text = x + ", " + z;
+                CreateMapPart(GetScreenPosition(x, y), CalculateButtonSize());
 
-                Transform createdMapPart = Instantiate(mapPart, GetWorldPosition(x, z), Quaternion.identity);
-                createdMapPart.localScale = CalculateBoxSize();
-
-                Debug.DrawLine(GetWorldPosition(x, z), GetWorldPosition(x, z + 1), Color.white, 100f);
-                Debug.DrawLine(GetWorldPosition(x, z), GetWorldPosition(x + 1, z), Color.white, 100f);
+                Debug.DrawLine(GetScreenPosition(x, y), GetScreenPosition(x, y + 1), Color.white, 100f);
+                Debug.DrawLine(GetScreenPosition(x, y), GetScreenPosition(x + 1, y), Color.white, 100f);
             }
         }
-        Debug.DrawLine(GetWorldPosition(0, height), GetWorldPosition(width, height), Color.white, 100f);
-        Debug.DrawLine(GetWorldPosition(width, 0), GetWorldPosition(width, height), Color.white, 100f);
+        Debug.DrawLine(GetScreenPosition(0, height), GetScreenPosition(width, height), Color.white, 100f);
+        Debug.DrawLine(GetScreenPosition(width, 0), GetScreenPosition(width, height), Color.white, 100f);
     }
 
-    public Vector3 GetWorldPosition(int x, int z)
+    public Vector2 GetScreenPosition(int x, int y)
     {
-        return new Vector3(x, 0, z) * cellSize;
+        return (new Vector2(x, y) * cellSize) + new Vector2(0f, screenHeightPixelValue);
     }
 
-    public void GetCellPosition(Vector3 worldPos, out int x, out int z)
+    public void GetCellPosition(Vector2 screenPos, out int x, out int y)
     {
-        x = Mathf.FloorToInt(worldPos.x / cellSize);
-        z = Mathf.FloorToInt(worldPos.z / cellSize);
-        Debug.Log(x + ", " + z + " - " + mapArray[x, z].ToString());
+        x = Mathf.FloorToInt(screenPos.x / cellSize.x);
+        y = Mathf.FloorToInt(screenPos.y / cellSize.y);
+        Debug.Log(x + ", " + y + " - " + mapArray[x, y].ToString());
     }
 
-    private Vector3 CalculateBoxSize()
+    private Vector2 CalculateButtonSize()
     {
-        Vector3 desiredSize = new Vector3(size, .1f, size);
-        return desiredSize;
+        return new Vector2(cellSize.x / buttonSizeMult, cellSize.y / buttonSizeMult);
+    }
+
+    private void CreateMapPart(Vector3 worldPos, Vector2 size)
+    {
+        Vector2 anchoredPos;
+        RectTransformUtility.ScreenPointToLocalPointInRectangle(gameInPanel, worldPos, cam, out anchoredPos);
+
+        RectTransform mapButtonPart = Instantiate(mapPart, Vector3.zero, Quaternion.identity);
+        mapButtonPart.parent = gameInPanel.transform;
+        mapButtonPart.localPosition = new Vector3(anchoredPos.x, anchoredPos.y, 0f);
+        mapButtonPart.localScale = Vector3.one;
+
+        RectTransform mapButton = mapButtonPart.transform.GetChild(0).GetComponent<RectTransform>();
+        mapButton.sizeDelta = size;
+        mapButton.anchoredPosition = new Vector3(size.x * .5f, size.y * .5f, 0f);
+
     }
 }
